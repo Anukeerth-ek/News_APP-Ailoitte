f1f6d0c63ad5e95dc5c49d62aee4b5d63ce2848e
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2025-07-17 17:47:44 +0530

    fix: fixed name mismatch in top headlines

--- src/app/components/TopHeadlines.tsx
+++ src/app/components/TopHeadlines.tsx
@@ -65,8 +65,8 @@ export const TopHeadlines = () => {
                </div>
 
                <div className="grid grid-cols-1 xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-                    {newsData.slice(1, visibleCount).map((news, idx) => (
-                         <NewsCard key={idx} news={news} />
+                    {newsData.slice(1, visibleCount).map((news, index) => (
+                         <NewsCard id={index} news={news} />
                     ))}
                </div>
 
